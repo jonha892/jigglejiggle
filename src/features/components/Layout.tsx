62ddf96edcfbd81035d7f0f059ce05b50901bd5e
jonha892 <jhalstrup@googlemail.com>
2024-05-25 13:11:18 +0200

    remove old mui file

--- src/features/components/Layout.tsx
+++
@@ -1,6 +0,0 @@
-import { Box } from '@mui/material'
-import { FC, PropsWithChildren } from 'react'
-
-export const Layout: FC<PropsWithChildren> = ({ children }) => {
-  return <Box sx={{ maxWidth: '1400px', marginLeft: 'auto', marginRight: 'auto' }}>{children}</Box>
-}
