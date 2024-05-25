639384c1a4501ffd12a9a6deb097a40a1bada90c
jonha892 <jhalstrup@googlemail.com>
2024-05-25 13:14:19 +0200

    remove old mui file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import { ConfigProvider } from 'antd'
 
 import { FC, PropsWithChildren } from 'react'
-import { Layout } from './features/components/Layout'
 
 export const App: FC<PropsWithChildren> = ({ children }) => {
-  return (
-    <ConfigProvider>
-      <Layout>{children}</Layout>
-    </ConfigProvider>
-  )
+  return <ConfigProvider>{children}</ConfigProvider>
 }
